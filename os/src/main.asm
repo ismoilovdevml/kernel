61eb27529749f331206f94e6b1254021c356aaa7
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-03-18 09:48:54 +0500

    updated

--- os/src/main.asm
+++ os/src/main.asm
@@ -1,28 +1,50 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x00, 0x0A
 
 
+start:
+	jmp main
 
 
+puts:
+	push si
+	push ax
 
+.loop:
+	lodsb
+	or al, al
+	jz .done
+	mov ah, 0x0e
+	int 0x10
+
+	jmp .loop
+
+.done:
+	pop ax
+	pop si
+	ret
+	 
 main:
-	; etup data segments
 	mov ax, 0
 	mov ds, ax
 	mov es, ax
 
 	; setup stack
-	mov ss, axmov sp, 0x7C00
+	mov ss, ax
+	mov sp, 0x7C00
 
+	mov si, msg_salom
+	call puts
 
-
-	
 	hlt
 	
 .halt:
 	jmp .halt
 
+msg_salom: db 'Salom Dasturchi', ENDL, 0
+
 
 times 510-($-$$) db 0
-dw 0AA55h
+dw 0xAA55
